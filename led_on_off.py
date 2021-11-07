4378034781f0cc47fd57a2f305759af95dc06b6b
NaumanWasti <87071367+NaumanWasti@users.noreply.github.com>
2021-11-07 22:18:55 +0500

    Add files via upload

---
+++ led_on_off.py
@@ -0,0 +1,13 @@
+import time
+import RPi.GPIO as GPIO
+GPIO.setmode(GPIO.BOARD)
+
+led = 5 
+GPIO.setup(led, GPIO.OUT, initial = 0) # Setup LED and set it initially to OFF
+
+while(True):
+        GPIO.output(led, GPIO.HIGH) # Set LED to ON
+        time.sleep(2) # Wait for 2 seconds
+
+        GPIO.output(led, GPIO.LOW) # Set LED to OFF
+        time.sleep(2) 
